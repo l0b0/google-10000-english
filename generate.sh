3e7c0570f8dab9a797e7dbaebd6816152ccb7710
Victor Engmark <victor.engmark@gmail.com>
2018-09-22 15:54:31 +1200

    Limit output lines with `shuf`

--- generate.sh
+++ generate.sh
@@ -4,4 +4,4 @@ set -o errexit -o nounset
 
 words="${1-2048}"
 
-printf '%s\n' "$(head -n "$words" words.txt | shuf | head --lines=4 | tr '\n' ' ')"
+printf '%s\n' "$(head -n "$words" words.txt | shuf --head-count=4 | tr '\n' ' ')"
